0e80fe08f67e008fb585180e7f9e3db6520a3c27
wasidc <wasid.chowdhury@icloud.com>
2025-09-25 13:57:10 -0400

    more notes

--- src/main/java/com/pluralsight/main.java
+++ src/main/java/com/pluralsight/main.java
@@ -74,5 +74,64 @@ public class main {
 
          */
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+
+        /*
+        if (age > 25) {
+            System.out.println("You are allowed to drink!");
+        } else {
+            System.out.println("You are not allowed to drink!");
+        }
+
+         */
+
+        /*
+        double price;
+
+        if (age <= 18) {
+            price = 22.50;
+        } else if (age <= 65) {
+            price = 30.50;
+        } else {
+            price = 25.00;
+
+        }
+
+        System.out.println("The price is: " + price);
+        */
+
+        /*
+        double price;
+        if (age < 18 || age >= 65) {
+            price = 18.00;
+        } else {
+            price = 25.00;
+        }
+
+        System.out.println("The price is: " + price);
+
+         */
+
+        /*
+        double price = (age < 18) ? 22.50 : 25.00;
+
+        System.out.println("The price is: " + price);
+
+         */
+
+        /*
+        if (name.equals("Raymond")) {
+            System.out.println("I love Potatoes!");
+        } else {
+            System.out.println("I don't love Potatoes!");
+
+         */
+        }
     }
 }
